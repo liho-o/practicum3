2ba6c03447522966714de706b3284e9115e73bed
liho-o <daniil200120@mail.ru>
2024-03-01 23:35:04 +0700

    8

---
+++ 8.py
@@ -0,0 +1,91 @@
+import turtle
+
+turtle.screensize(900, 900)
+turtle.speed(100)
+
+
+
+turtle.pencolor('#1FBBEB')  # Установка цвета заливки
+turtle.width(9)
+
+turtle.up()
+turtle.setposition(-121, 220)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+turtle.forward(170)  # Переместиться вперед на длину стороны
+
+
+turtle.up()
+turtle.setposition(-131, 260)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+turtle.forward(195)  # Переместиться вперед на длину стороны
+
+turtle.up()
+turtle.setposition(-101, 280)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+turtle.right(72)  # Повернуть
+turtle.forward(95)  # Переместиться вперед на длину стороны
+
+
+turtle.up()
+turtle.setposition(-31, 280)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+turtle.right(17)  # Повернуть
+turtle.forward(92)  # Переместиться вперед на длину стороны
+
+turtle.up()
+turtle.setposition(41, 280)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+turtle.right(16)  # Повернуть
+turtle.forward(90)  # Переместиться вперед на длину стороны
+
+
+
+turtle.width(8)
+turtle.pencolor('#004C99')  # Установка цвета заливки
+
+turtle.up()  # Поднять перо (не оставлять след при перемещении)
+turtle.setposition(-180, 300)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+
+turtle.right(-105)  # Повернуть
+
+turtle.forward(30)  # Переместиться вперед на длину стороны
+turtle.right(70)  # Повернуть
+
+turtle.forward(100)  # Переместиться вперед на длину стороны
+turtle.right(180)  # Повернуть
+
+turtle.forward(80)  # Переместиться вперед на длину стороны
+
+turtle.right(110)  # Повернуть
+turtle.forward(220)  # Переместиться вперед на длину стороны
+
+turtle.right(110)  # Повернуть
+turtle.forward(100)  # Переместиться вперед на длину стороны
+
+turtle.right(70)  # Повернуть
+turtle.forward(150)  # Переместиться вперед на длину стороны
+
+turtle.left(110)  # Повернуть
+turtle.forward(25)  # Переместиться вперед на длину стороны
+
+turtle.left(70)  # Повернуть
+turtle.forward(135)  # Переместиться вперед на длину стороны
+
+
+turtle.up()
+turtle.setposition(10, 130)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+
+turtle.dot(30)
+
+turtle.up()
+turtle.setposition(-70, 130)  # Установка позиции
+turtle.down()  # Опустить перо (оставлять след при перемещении)
+
+turtle.dot(30)
+
+
+
+
+turtle.done()
